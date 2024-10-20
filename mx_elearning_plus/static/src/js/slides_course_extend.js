0374b734ced4f7a0a6326a39b8d794a6c8d092ea
Eng-Abdelhakeem Qotob <34494981+abdelhakiam@users.noreply.github.com>
2024-10-20 07:41:29 +0300

    Update slides_course_extend.js

--- mx_elearning_plus/static/src/js/slides_course_extend.js
+++ mx_elearning_plus/static/src/js/slides_course_extend.js
@@ -1,6 +1,8 @@
 odoo.define('mx_elearning_plus.course_extended', function (require) {
     'use strict';
+    
     var publicWidget = require('web.public.widget');
+    
     publicWidget.registry.websiteCourseExtended = publicWidget.Widget.extend({
         selector: '.o_course_extended',
 
@@ -8,16 +10,19 @@ odoo.define('mx_elearning_plus.course_extended', function (require) {
          * @override
          */
         start: function () {
-            $('.o_course_extended').on('click','#collapse_div',function() {
-                var id = this.dataset.target.split('-')[1]
-                if($('#slide-'+id).hasClass('show')) {
-                    $(this).children().first().children().removeClass('fa-minus');
-                    $(this).children().first().children().addClass('fa-plus');
-                }else {
-                    $(this).children().first().children().removeClass('fa-plus');
-                    $(this).children().first().children().addClass('fa-minus');
+            var self = this; // تخزين السياق
+
+            this.$el.on('click', '#collapse_div', function () {
+                var id = this.dataset.target.split('-')[1];
+                var $slide = $('#slide-' + id);
+                var $icon = $(this).find('i'); // العثور على الأيقونة داخل العنصر
+
+                if ($slide.hasClass('show')) {
+                    $icon.removeClass('fa-minus').addClass('fa-plus');
+                } else {
+                    $icon.removeClass('fa-plus').addClass('fa-minus');
                 }
             });
         }
-    })
+    });
 });
