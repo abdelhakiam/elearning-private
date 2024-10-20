2bfa38b10ada251d6c1bcc3afe818a180b363ddf
Eng-Abdelhakeem Qotob <34494981+abdelhakiam@users.noreply.github.com>
2024-10-20 07:33:06 +0300

    Update __manifest__.py

--- elearning_private_youtube_video/__manifest__.py
+++ elearning_private_youtube_video/__manifest__.py
@@ -1,39 +1,29 @@
 # -*- coding: utf-8 -*-
 {
     'name': "elearning private youtube video",
-
-    'summary': """elearning private youtube video
-    """,
-
+    'summary': """elearning private youtube video""",
     'description': """
        elearning private youtube video
     """,
-
     'author': 'David Montero Crespo',
     'license': 'AGPL-3',
     'category': 'Website',
-    'version': '13.0',
-'website': "https://softwareescarlata.com/",
-    # any module necessary for this one to work correctly
-    'depends': ['base','website_slides'],
+    'version': '15.0.1.0.0',
+    'website': "https://softwareescarlata.com/",
+    'depends': ['base', 'website_slides', 'web'],
     'images': ['static/description/imagen.jpg'],
-    # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-
-
-
+        'views/templates.xml',  # إضافة ملف القوالب
     ],
-    "assets": {
-        "web.assets_frontend": [
-            "elearning_private_youtube_video/static/scss/style.scss",
-            "elearning_private_youtube_video/static/js/slides_course_fullscreen_player_video.js",
-            ],
-
+    'assets': {
+        'web.assets_frontend': [
+            'elearning_private_youtube_video/static/scss/style.scss',
+            'elearning_private_youtube_video/static/js/slides_course_fullscreen_player_video.js',
+        ],
     },
-
     'currency': 'EUR',
     'price': 25,
-    # only loaded in demonstration mode
-
+    'installable': True,
+    'application': False,
 }
